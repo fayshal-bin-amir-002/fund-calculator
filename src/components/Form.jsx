bc3f788a3e1ea2694d9d260b5e02631235e6c204
fayshal69 <foyshalbinamir@gmail.com>
2024-09-09 22:59:12 +0600

    added: form section

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,6 +10,22 @@ const Form = () => {
                     <label htmlFor="amount" className="text-xl font-medium">Amount : </label>
                     <input type="number" name="amount" id="amount" className="border border-sky-400 p-2 rounded-lg w-2/3" placeholder="Enter your amount here..." />
                 </div>
+                <div className="flex justify-between items-center pb-5">
+                    <label className="text-xl font-medium">Type : </label>
+                    <div className="flex w-2/3 justify-start items-center gap-12">
+                        <div>
+                            <input type="radio" name="type" id="type" value="income" className="mr-2" />
+                            <label htmlFor="type">Income</label>
+                        </div>
+                        <div>
+                            <input type="radio" name="type" id="type" value="expense" className="mr-2" />
+                            <label htmlFor="type">Expense</label>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <button className="w-full p-3 bg-sky-200 text-lg font-medium rounded-lg hover:scale-105 duration-500">Add Fund</button>
+                </div>
             </form>
         </div>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Form from "../components/Form";
 
 const Home = () => {
     return (
-        <div className="xl:w-1/3 md:w-2/3 bg-gray-50 h-[90%] w-full rounded-lg shadow-lg overflow-hidden">
+        <div className="xl:w-1/3 md:w-2/3 bg-gray-50 min-h-[70vh] w-full rounded-lg shadow-lg overflow-hidden">
             <Balance></Balance>
             <Form></Form>
         </div>
