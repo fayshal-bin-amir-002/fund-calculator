9b240b1033c1120e5f3441846fe014bb4150badb
fayshal69 <foyshalbinamir@gmail.com>
2024-09-09 22:27:06 +0600

    added: navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,9 @@
 const NavBar = () => {
     return (
         <nav className="py-3 md:py-5 lg:py-7 bg-sky-300">
-            <h3 className="text-center text-2xl md:text-3xl lg:text-4xl font-semibold text-gray-700">Fund Calculator</h3>
+            <h3 className="text-center text-2xl md:text-3xl lg:text-4xl font-semibold text-gray-700">
+                Fund Calculator
+            </h3>
         </nav>
     );
 };
