feefb146c41a2420c9f55c3427bd074621239442
fayshal69 <foyshalbinamir@gmail.com>
2024-09-10 15:23:33 +0600

    update: login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,11 @@
 import { useContext } from "react";
 import { AuthContext } from "../AuthProvider/AuthProvider";
 import { Navigate } from "react-router-dom";
+import { CgSpinnerTwo } from "react-icons/cg";
 
 const Login = () => {
 
-    const { user, userLogin } = useContext(AuthContext);
+    const { user, userLogin, loading } = useContext(AuthContext);
 
     const handleLogin = async (e) => {
         e.preventDefault();
@@ -33,7 +34,8 @@ const Login = () => {
                 </div>
 
                 <div>
-                    <button className="flex flex-no-wrap mt-6 border-2 border-white text-white bg-sky-500 py-2 px-4" type="submit">
+                    <button className="flex flex-no-wrap mt-6 border-2 border-white text-white bg-sky-500 py-2 px-4 disabled:cursor-not-allowed" type="submit" disabled={loading}>
+                        { loading && <span><CgSpinnerTwo className="inline animate-spin text-lg mr-2" /></span> }
                         <span>Take Me In</span>
                         <span>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="ml-2 w-6 h-6"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
